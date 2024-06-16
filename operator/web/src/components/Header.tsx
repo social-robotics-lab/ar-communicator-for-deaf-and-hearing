358a6b07c784bbd31474c5e9e7139a343509eb03
ayakakawabe <kawabe.ayaka@icloud.com>
2024-06-16 18:24:24 +0900

    add header in operator

---
+++ operator/web/src/components/Header.tsx
@@ -0,0 +1,11 @@
+import "./Header.css"
+
+const Header:React.FC=()=>{
+    return(
+        <div className="header">
+            <h1>For Operator of AR Communicator</h1>
+        </div>
+    )
+};
+
+export default Header;
--- operator/web/src/main.tsx
+++ operator/web/src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import Home from './pages/Home.tsx'
+import Header from './components/Header.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Header />
+    <Home />
   </React.StrictMode>,
 )
