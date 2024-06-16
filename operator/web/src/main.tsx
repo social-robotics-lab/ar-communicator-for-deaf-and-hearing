22d79abfc3f2f82051bb20c5ab7b77db38e6b377
ayakakawabe <kawabe.ayaka@icloud.com>
2024-06-16 19:26:04 +0900

    add home folder

--- operator/web/src/main.tsx
+++ operator/web/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Home from './pages/Home.tsx'
+import Home from './pages/home/Home.tsx'
 import Header from './components/header/Header.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
