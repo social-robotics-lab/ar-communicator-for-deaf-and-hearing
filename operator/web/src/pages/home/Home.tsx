768ce5edcc36e4943cdf42544b69b6735f6941b5
ayakakawabe <kawabe.ayaka@icloud.com>
2024-06-16 19:26:04 +0900

    add home folder

--- operator/web/src/pages/home/Home.tsx
+++ operator/web/src/pages/home/Home.tsx
@@ -1,4 +1,4 @@
-import UserSelectButton from "../components/userSelectButton/UserSelectButton";
+import UserSelectButton from "../../components/userSelectButton/UserSelectButton";
 import "./Home.css"
 
 const Home:React.FC=()=>{
