caf951a0518bcf39475e2aeb9aa426600ea4f2cf
ayakakawabe <kawabe.ayaka@icloud.com>
2024-06-17 13:35:41 +0900

    add router

---
+++ operator/web/src/pages/user1/User1.tsx
@@ -0,0 +1,9 @@
+const User1:React.FC=()=>{
+    return(
+        <div>
+            <h2>User1</h2>
+        </div>
+    )
+};
+
+export default User1;
---
+++ operator/web/src/pages/user2/User2.tsx
@@ -0,0 +1,9 @@
+const User2:React.FC=()=>{
+    return(
+        <div>
+            <h2>User2</h2>
+        </div>
+    )
+};
+
+export default User2;
