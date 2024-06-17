3046339507daf7f004a6ab8ec856d27741ea8a90
ayakakawabe <kawabe.ayaka@icloud.com>
2024-06-17 13:35:41 +0900

    add router

--- operator/web/src/main.tsx
+++ operator/web/src/main.tsx
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Home from './pages/home/Home.tsx'
 import Header from './components/header/Header.tsx'
+import Router from './routers/Router.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Header />
-    <Home />
+    <Router />
   </React.StrictMode>,
 )
---
+++ operator/web/src/pages/user3/User3.tsx
@@ -0,0 +1,9 @@
+const User3:React.FC=()=>{
+    return(
+        <div>
+            <h2>User3</h2>
+        </div>
+    )
+};
+
+export default User3;
