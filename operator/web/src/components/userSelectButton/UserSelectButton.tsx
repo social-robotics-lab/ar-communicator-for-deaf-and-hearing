4952ccf0668c5080b021baf165e1e5f7228cbe10
ayakakawabe <kawabe.ayaka@icloud.com>
2024-06-17 13:46:07 +0900

    add link to each user page

--- operator/web/src/components/userSelectButton/UserSelectButton.tsx
+++ operator/web/src/components/userSelectButton/UserSelectButton.tsx
@@ -1,12 +1,15 @@
+import { Link } from "react-router-dom";
 import "./UserSelectButton.css"
 
 const UserSelectButton:React.FC<{userIndex:number,isDHH:boolean}>=({userIndex,isDHH})=>{
     return(
         <div id="userSelectButton">
-            <button>
-                <h3>User {userIndex}</h3>
-                <p>is DHH : {String(isDHH)}</p>
-            </button>
+            <Link to={"/user"+String(userIndex)}>
+                <button>
+                    <h3>User {userIndex}</h3>
+                    <p>is DHH : {String(isDHH)}</p>
+                </button>
+            </Link>
         </div>
     )
 };
--- operator/web/src/main.tsx
+++ operator/web/src/main.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Home from './pages/home/Home.tsx'
 import Header from './components/header/Header.tsx'
 import Router from './routers/Router.tsx'
 
