11ad99d82946c239280c4114b046ef79afe43912
ayakakawabe <kawabe.ayaka@icloud.com>
2024-06-17 18:32:17 +0900

    add provider data in each user page

--- operator/web/src/pages/user/User2.tsx
+++ operator/web/src/pages/user/User2.tsx
@@ -1,10 +1,13 @@
+import { useContext } from "react";
 import ScenarioButton from "../../components/ scenarioButton/ ScenarioButton";
 import UserHeader from "../../components/userHeader/UserHeader";
+import { UsersInfoContext } from "../../providers/UsersInfoProvider";
 
 const User2:React.FC=()=>{
+    const {usersInfo}=useContext(UsersInfoContext);
     return(
         <div className="user">
-            <UserHeader userIndex={2} isDHH={true} />
+            <UserHeader userIndex={2} isDHH={usersInfo.user2.isDHH} />
             <div>
                 <ScenarioButton scenario="シナリオ" />
             </div>
--- operator/web/src/pages/user/User3.tsx
+++ operator/web/src/pages/user/User3.tsx
@@ -1,10 +1,13 @@
+import { useContext } from "react";
 import ScenarioButton from "../../components/ scenarioButton/ ScenarioButton";
 import UserHeader from "../../components/userHeader/UserHeader";
+import { UsersInfoContext } from "../../providers/UsersInfoProvider";
 
 const User3:React.FC=()=>{
+    const {usersInfo}=useContext(UsersInfoContext);
     return(
         <div className="user">
-            <UserHeader userIndex={3} isDHH={true} />
+            <UserHeader userIndex={3} isDHH={usersInfo.user3.isDHH} />
             <div>
                 <ScenarioButton scenario="シナリオ" />
             </div>
