a438c4bd4cb8d520ffbbe7e133c91b0adaa5458c
ayakakawabe <kawabe.ayaka@icloud.com>
2024-06-17 23:04:00 +0900

    get firebase data

--- operator/web/src/firebase/FirebaseConfig.ts
+++ operator/web/src/firebase/FirebaseConfig.ts
@@ -1,25 +1,29 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getDatabase } from "firebase/database";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const apiKey=import.meta.env.VITE_FIREBASE_APIKEY;
+const authDomain=import.meta.env.VITE_FIREBASE_AUTHDOMAIN;
+const databaseURL=import.meta.env.VITE_FIREBASE_DATABASEURL;
+const projectId=import.meta.env.VITE_FIREBASE_PROJECTID;
+const storageBucket=import.meta.env.VITE_FIREBASE_STORAGEBUCKET;
+const messagingSenderId=import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID;
+const appId=import.meta.env.VITE_FIREBASE_APPID;
+const measurementId=import.meta.env.VITE_FIREBASE_MEASUREMENTID;
+
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
-  databaseURL: import.meta.env.VITE_FIREBASE_DATABASEURL,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID,
-  appId: import.meta.env.VITE_FIREBASE_APPID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENTID
+  apiKey: apiKey,
+  authDomain: authDomain,
+  databaseURL: databaseURL,
+  projectId: projectId,
+  storageBucket: storageBucket,
+  messagingSenderId: messagingSenderId,
+  appId: appId,
+  measurementId: measurementId
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 
-// Initialize Realtime Database and get a reference to the service
-const database = getDatabase(app);
+export const db = getDatabase(app);
