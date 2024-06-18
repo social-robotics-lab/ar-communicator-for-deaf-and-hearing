8fdc0dcd8219d75a8bdffe8ffb175d2481ab974e
ayakakawabe <kawabe.ayaka@icloud.com>
2024-06-18 19:33:30 +0900

    add input scenario data form

--- operator/web/src/pages/user/User1.tsx
+++ operator/web/src/pages/user/User1.tsx
@@ -3,12 +3,14 @@ import ScenarioButton from "../../components/ scenarioButton/ ScenarioButton";
 import UserHeader from "../../components/userHeader/UserHeader";
 import { useContext } from "react";
 import { UsersInfoContext } from "../../providers/UsersInfoProvider";
+import InputScenarioFileForm from "../../components/inputScenarioFileForm/InputScenarioFileForm";
 
 const User1:React.FC=()=>{
     const {usersInfo}=useContext(UsersInfoContext);
     return(
         <div className="user">
             <UserHeader userIndex={1} isDHH={usersInfo.user1.isDHH} />
+            <InputScenarioFileForm />
             <div>
                 <ScenarioButton userKey="user1" scenario="シナリオ１−１" />
             </div>
