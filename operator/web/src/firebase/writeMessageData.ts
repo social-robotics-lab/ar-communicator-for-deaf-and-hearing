add7b30679583e115a909f648b74f0ed5d22146d
ayakakawabe <kawabe.ayaka@icloud.com>
2024-06-18 15:34:51 +0900

    refactor comment out

--- operator/web/src/firebase/writeMessageData.ts
+++ operator/web/src/firebase/writeMessageData.ts
@@ -1,7 +1,5 @@
 import { ref, set } from "firebase/database";
 import { db } from "./firebaseConfig";
-import { useContext } from "react";
-import { UsersInfoContext } from "../providers/UsersInfoProvider";
 
 export const writeMessageData=(userKey:string,isDHH:boolean,message:string)=>{
     set(ref(db,userKey+"/"),{
