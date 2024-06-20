42938cf9abf23fc2bd1066bd44bf0da221c8429a
ayakakawabe <kawabe.ayaka@icloud.com>
2024-06-20 18:23:04 +0900

    add scenario button in User2,3

--- operator/web/src/pages/user/User2.tsx
+++ operator/web/src/pages/user/User2.tsx
@@ -1,15 +1,29 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import ScenarioButton from "../../components/ scenarioButton/ ScenarioButton";
 import UserHeader from "../../components/userHeader/UserHeader";
 import { UsersInfoContext } from "../../providers/UsersInfoProvider";
+import InputScenarioFileForm, { ScenarioListType } from "../../components/inputScenarioFileForm/InputScenarioFileForm";
 
 const User2:React.FC=()=>{
     const {usersInfo}=useContext(UsersInfoContext);
+    const [scenarioList,setScenarioList]=useState<ScenarioListType>();
     return(
         <div className="user">
             <UserHeader userIndex={2} isDHH={usersInfo.user2.isDHH} />
+            <InputScenarioFileForm setScenarioList={setScenarioList} />
             <div>
-                <ScenarioButton userKey="user2" scenario="シナリオ２-1" />
+                {scenarioList?.map((scenario,index)=>{
+                    return (
+                    <span key={index}>
+                        {scenario.user===2?
+                            <ScenarioButton userKey="user2" scenario={scenario.message} />
+                            :<div className="otherMessage">
+                                <span className="otherData">user{scenario.user}</span>
+                                <span className="otherData">{scenario.message}</span>
+                            </div>}
+                    </span>
+                    )
+                })}
             </div>
         </div>
     )
