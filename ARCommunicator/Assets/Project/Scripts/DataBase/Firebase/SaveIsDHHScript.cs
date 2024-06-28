6be2be84c770c742ab682bf18caa8706d5121e79
ayakakawabe <kawabe.ayaka@icloud.com>
2024-06-28 16:54:04 +0900

    add button

--- ARCommunicator/Assets/Project/Scripts/DataBase/Firebase/SaveIsDHHScript.cs
+++ ARCommunicator/Assets/Project/Scripts/DataBase/Firebase/SaveIsDHHScript.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using Firebase;
 using Firebase.Database;
+using UnityEngine.UI;
 
 public class SaveIsDHHScript : MonoBehaviour
 {
+    public Button TrueButton;
+    public Button FalseButton;
+
+    public string userId;
     private DatabaseReference databaseReference;
     // Start is called before the first frame update
     void Start()
     {
         // Get the root reference location of the database.
         databaseReference = FirebaseDatabase.DefaultInstance.RootReference;
+
+        TrueButton.onClick.AddListener(()=>WriteIsDHH(userId,true));
+        FalseButton.onClick.AddListener(() => WriteIsDHH(userId, false));
     }
 
     // Update is called once per frame
@@ -20,7 +28,7 @@ public class SaveIsDHHScript : MonoBehaviour
         
     }
 
-    private void WriteIsDHH(string userId, bool newIsDHH)
+    public void WriteIsDHH(string userId, bool newIsDHH)
     {
         DatabaseReference isDHHReference = databaseReference.Child(userId).Child("isDHH");
         isDHHReference.SetValueAsync(newIsDHH);
