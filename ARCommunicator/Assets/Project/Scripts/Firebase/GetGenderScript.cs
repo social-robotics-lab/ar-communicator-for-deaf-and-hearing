6785999d3a4d84adeda84643b35094d066905c6d
ayakakawabe <kawabe.ayaka@icloud.com>
2024-07-20 17:39:12 +0900

    add get gender script

---
+++ ARCommunicator/Assets/Project/Scripts/Firebase/GetGenderScript.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Database;
+using static Firebase.Extensions.TaskExtension;
+using UnityEngine.UI;
+using System.Threading.Tasks;
+
+public class GetGenderScript
+{
+    private DatabaseReference reference;
+
+    public async Task<string> GetGenderAsync(string userId)
+    {
+        string result = null;
+
+        reference = FirebaseDatabase.DefaultInstance.RootReference;
+
+        await reference.Child(userId).Child("gender")
+          .GetValueAsync().ContinueWithOnMainThread(task => {
+
+              if (task.IsFaulted)
+              {
+                  // Handle the error...
+                  Debug.Log("Could not read Firebase a static snapshot");
+              }
+              else if (task.IsCompleted)
+              {
+                  DataSnapshot snapshot = task.Result;
+                  // Do something with snapshot...
+                  // Debug.Log($"{userId} is DHH: {snapshot.Value}");
+                  result = (string)snapshot.Value;
+              }
+          });
+
+        if (!string.IsNullOrWhiteSpace(result))
+        {
+            return result;
+        }
+        else
+        {
+            throw new System.ArgumentNullException($"{userId} gender is null");
+
+        }
+    }
+}
--- ARCommunicator/Assets/Project/Scripts/SelectAvatarScript.cs
+++ ARCommunicator/Assets/Project/Scripts/SelectAvatarScript.cs
@@ -10,10 +10,16 @@ public class SelectAvatarScript : MonoBehaviour
     public GameObject Avatar4;
 
     // Start is called before the first frame update
-    void Start()
+    async void Start()
     {
         Destroy(Avatar1);
         Destroy(Avatar2);
+
+        GetGenderScript GetGender = new GetGenderScript();
+
+        string gender;
+        gender = await GetGender.GetGenderAsync("user1");
+        Debug.Log(gender);
     }
 
     // Update is called once per frame
