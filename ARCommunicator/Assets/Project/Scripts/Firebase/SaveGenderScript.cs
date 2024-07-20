29f2aa96a5876ec4a0bd5e5ebb5cf7b72ef2edaa
ayakakawabe <kawabe.ayaka@icloud.com>
2024-07-20 17:22:29 +0900

    write gender data in firebase

---
+++ ARCommunicator/Assets/Project/Scripts/Firebase/SaveGenderScript.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Database;
+using UnityEngine.UI;
+
+
+public class SaveGenderScript : MonoBehaviour
+{
+    public Button femaleButton;
+    public Button maleButton;
+
+    public string userId;
+    private DatabaseReference databaseReference;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get the root reference location of the database.
+        databaseReference = FirebaseDatabase.DefaultInstance.RootReference;
+
+        femaleButton.onClick.AddListener(() => WriteIsDHH(userId, "f"));
+        maleButton.onClick.AddListener(() => WriteIsDHH(userId, "m"));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void WriteIsDHH(string userId, string newGender)
+    {
+        DatabaseReference isDHHReference = databaseReference.Child(userId).Child("gender");
+        isDHHReference.SetValueAsync(newGender);
+    }
+}
