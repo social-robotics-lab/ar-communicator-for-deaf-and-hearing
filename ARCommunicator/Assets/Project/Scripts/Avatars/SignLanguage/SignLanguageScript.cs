9107e5fe96b8ecc8366461e95e58f2d7fc1f1939
keito <kate.kyoto.118@gmail.com>
2024-08-07 14:27:00 +0900

    MotionTriggerのパラメータ追加

--- ARCommunicator/Assets/Project/Scripts/Avatars/SignLanguage/SignLanguageScript.cs
+++ ARCommunicator/Assets/Project/Scripts/Avatars/SignLanguage/SignLanguageScript.cs
@@ -6,15 +6,48 @@ public class SignLanguageScript : MonoBehaviour
 {
     private GameObject Avatar;
 
+    // Animatorの参照を追加
+    public Animator animator;
+    public string animatorParameterName = "SignMessageId"; // ここにAnimatorのパラメーター名を指定
+    private Dictionary<int, string> ScenarioIdMessageDict; // ScenarioIdMessageDictを定義
+
     private void Start()
     {
         Avatar = this.gameObject;
+
+        // ScenarioToDictを初期化し、辞書を取得
+        ScenarioToDict scenarioToDict = new ScenarioToDict();
+        Dictionary<int, string> dict = scenarioToDict.GetScenarioDictionary();
+        ScenarioIdMessageDict = dict; // ScenarioIdMessageDictに辞書を設定
     }
 
     public void SignLanguage(string message)
     {
         Debug.Log($"this avatar is {Avatar.name}\nSignLanguageメソッド(message: {message})");
-        // TODO: 相手のアバター(Avatar)に手話させる
+
+        foreach (KeyValuePair<int, string> kvp in ScenarioIdMessageDict)
+        {
+            if (kvp.Value == message)
+            {
+                // AnimatorのパラメーターにIDを設定
+                if (animator != null)
+                {
+                    animator.SetInteger(animatorParameterName, kvp.Key);
+                    animator.SetTrigger("MotionTrigger"); // トリガーをONにする
+                    Debug.Log("Animator parameter set to ID: " + kvp.Key);
+
+                }
+                else
+                {
+                    Debug.LogError("Animator is not assigned.");
+                }
+
+                return;
+            }
+        }
+
+        Debug.Log("Message not found in dictionary.");
     }
+
 
 }
